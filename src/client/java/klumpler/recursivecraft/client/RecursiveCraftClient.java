3b2067672cff520cd86a89bd06adbac7ef753174
GoldOliverK09 <oliver.klumpler@outlook.com>
2026-07-29 15:27:39 +0100

    Initial Fabric 26.2 template

---
+++ src/client/java/klumpler/recursivecraft/client/RecursiveCraftClient.java
@@ -0,0 +1,10 @@
+package klumpler.recursivecraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RecursiveCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
